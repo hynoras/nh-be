6030da254e6da7529d39af094ec232cb43e2804c
quang <hynoras1@gmail.com>
2026-02-15 13:08:54 +0700

    refactor: standardize error files

--- internal/features/user/error.go
+++ internal/features/user/error.go
@@ -3,16 +3,14 @@ package user
 import "errors"
 
 var (
-	ErrUserNotFound      = errors.New("user not found")
-	ErrDuplicateUsername = errors.New("username already exists")
-	ErrDuplicateEmail    = errors.New("email already exists")
-	ErrForbidCreateUser  = errors.New("you do not have permission to create users")
-	ErrForbidViewUsers   = errors.New("you do not have permission to view users")
-	ErrForbidViewUser    = errors.New("you do not have permission to view this user")
-	ErrForbidUpdateUser  = errors.New("you do not have permission to update this user")
-	ErrForbidDeleteUser  = errors.New("you do not have permission to delete this user")
-
-	// Username validation errors
+	ErrUserNotFound                      = errors.New("user not found")
+	ErrDuplicateUsername                 = errors.New("username already exists")
+	ErrDuplicateEmail                    = errors.New("email already exists")
+	ErrForbidCreateUser                  = errors.New("you do not have permission to create users")
+	ErrForbidViewUsers                   = errors.New("you do not have permission to view users")
+	ErrForbidViewUser                    = errors.New("you do not have permission to view this user")
+	ErrForbidUpdateUser                  = errors.New("you do not have permission to update this user")
+	ErrForbidDeleteUser                  = errors.New("you do not have permission to delete this user")
 	ErrInvalidUsernameLength             = errors.New("username must be between 3 and 30 characters")
 	ErrInvalidUsernameChars              = errors.New("username can only contain letters, numbers, dots, and underscores")
 	ErrUsernameMustStartWithLetter       = errors.New("username must start with a letter")
