638164cfe2220492d1ed0b765a8e21a5f1741c24
Quách Vinh Quang <hynoras1@gmail.com>
2026-05-10 11:44:39 +0700

    db(migration): create initial schemas

---
+++ migrations/000001_init_schema.down.sql
@@ -0,0 +1,13 @@
+-- 000001_init_schema.down.sql
+
+DROP TABLE IF EXISTS observations;
+DROP TABLE IF EXISTS experiment_results;
+DROP TABLE IF EXISTS experiments;
+DROP TABLE IF EXISTS procedure_steps;
+DROP TABLE IF EXISTS procedures;
+DROP TABLE IF EXISTS verification_tokens;
+DROP TABLE IF EXISTS user_permissions;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS permission_group_items;
+DROP TABLE IF EXISTS permission_groups;
+DROP TABLE IF EXISTS permissions;
