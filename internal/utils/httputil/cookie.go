f264feb72f27d3d11fab285cd569b8d062c28018
Quách Vinh Quang <hynoras1@gmail.com>
2026-05-13 13:33:22 +0700

    refactor(cookie): disable SameSite in production

--- internal/utils/httputil/cookie.go
+++ internal/utils/httputil/cookie.go
@@ -3,18 +3,29 @@ package httputil
 import (
 	"net/http"
 	"nh-be/internal/constant"
+	"nh-be/pkg/env"
 )
 
 // GetAuthSessionCookie returns a new http.Cookie configured for authentication sessions
 // with the provided session ID.
 func GetAuthSessionCookie(sessionId string) *http.Cookie {
+	sameSite := http.SameSiteLaxMode
+	secure := false
+
+	// If in production, enable cross-site cookie support
+	if env.GetEnvOrDefault("APP_ENV", "dev") == "prod" {
+		sameSite = http.SameSiteNoneMode
+		secure = true
+	}
+
 	return &http.Cookie{
 		Name:     constant.AuthSessionCookieName,
 		Value:    sessionId,
 		Path:     constant.AuthSessionCookiePath,
 		HttpOnly: true,
-		Secure:   true,
-		SameSite: http.SameSiteLaxMode,
+		Secure:   secure,
+		SameSite: sameSite,
 		MaxAge:   constant.AuthSessionCookieMaxAge,
 	}
 }
+
