a8588368adad0eadb8bed5672e9b2f117f35febd
Quách Vinh Quang <hynoras1@gmail.com>
2026-06-01 22:11:52 +0700

    feat: add more fields to experiment schema to satisfy business requirement

--- internal/features/experiment/model.go
+++ internal/features/experiment/model.go
@@ -10,30 +10,41 @@ import (
 )
 
 type Experiment struct {
-	ID        uuid.UUID        `gorm:"type:uuid;primaryKey"`
-	Title     string           `gorm:"not null"`
-	Objective string           `gorm:"type:text"`
-	Status    ExperimentStatus `gorm:"type:varchar(20);not null;index;default:draft"`
-	Type      ExperimentType   `gorm:"type:varchar(20);not null;index"`
-	Version   int              `gorm:"not null;default:1"`
+	ID uuid.UUID `gorm:"type:uuid;primaryKey"`
 
-	CreatedByID uuid.UUID `gorm:"type:uuid;not null;index"`
-	CreatedBy   user.User `gorm:"foreignKey:CreatedByID"`
+	// Human-readable identifier
+	Identifier string `gorm:"type:varchar(50);uniqueIndex;not null"`
 
+	// Metadata
+	Title     string `gorm:"type:varchar(200);not null;uniqueIndex:idx_user_experiment_title"` //composite key with CreatedByID
+	Objective string `gorm:"type:text"`
+
+	// Workflow
+	Status ExperimentStatus `gorm:"type:varchar(20);not null;index"`
+	Type   ExperimentType   `gorm:"type:varchar(20);not null;index"`
+
+	// Optimistic locking
+	Version int `gorm:"not null;default:1"`
+
+	// Ownership
+	CreatedByID uuid.UUID `gorm:"not null;uniqueIndex:idx_user_experiment_title"` //composite key with Title
+	CreatedBy   user.User
+
+	UpdatedByID *uuid.UUID `gorm:"type:uuid;index"`
+	UpdatedBy   *user.User
+
+	// Scheduling
+	PlannedStartAt *time.Time
+	PlannedEndAt   *time.Time
+
+	// Execution
 	StartedAt   *time.Time
 	CompletedAt *time.Time
 
+	// Procedure template
+	ProcedureID *uuid.UUID `gorm:"type:uuid;index"`
+	Procedure   proc.Procedure
+
 	CreatedAt time.Time
 	UpdatedAt time.Time
-
-	ProcedureID *uuid.UUID     `gorm:"type:uuid;index"`
-	Procedure   proc.Procedure `gorm:"foreignKey:ProcedureID"`
-
-	// Relations
-	// Steps        []ExperimentStep        `gorm:"constraint:OnDelete:CASCADE"`
-	// Materials    []ExperimentMaterial    `gorm:"constraint:OnDelete:CASCADE"`
-	// Conditions   []ExperimentCondition   `gorm:"constraint:OnDelete:CASCADE"`
-	// Observations []ExperimentObservation `gorm:"constraint:OnDelete:CASCADE"`
-	// Results      []ExperimentResult      `gorm:"constraint:OnDelete:CASCADE"`
-	// Notes        []ExperimentNote        `gorm:"constraint:OnDelete:CASCADE"`
 }
